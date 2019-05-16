5b275896069cda33b9b6fffa987b8b0347d046a0
WielkiB <wielkib213@gmail.com>
2019-05-16 12:02:54 +0200

    dodanie emaila

--- FreeT.sql
+++ FreeT.sql
@@ -40,9 +40,9 @@ INSERT INTO Zespol(nazwa) VALUES ('Zespol1')
 INSERT INTO Zespol(nazwa) VALUES ('Zespol2')
 
 
-INSERT INTO Uzytkownik(login,imie,nazwisko,haslo,zespol_id) VALUES('Login1','Jan','Kowalski','password', '1')
-INSERT INTO Uzytkownik(login,imie,nazwisko,haslo,zespol_id) VALUES('Login2','Tomasz','Krab','password', '1')
-INSERT INTO Uzytkownik(login,imie,nazwisko,haslo,zespol_id) VALUES('Login3','Łukasz','Nowak','password', '2')
+INSERT INTO Uzytkownik(login,imie,nazwisko,haslo,zespol_id) VALUES('Login1','Jan','Kowalski','5F4DCC3B5AA765D61D8327DEB882CF99', '1')
+INSERT INTO Uzytkownik(login,imie,nazwisko,haslo,zespol_id) VALUES('Login2','Tomasz','Krab','5F4DCC3B5AA765D61D8327DEB882CF99', '1')
+INSERT INTO Uzytkownik(login,imie,nazwisko,haslo,zespol_id) VALUES('Login3','Łukasz','Nowak','5F4DCC3B5AA765D61D8327DEB882CF99', '2')
 
 INSERT INTO Urlop(dataod,datado,uzytkownik_id) VALUES('2019-05-20','2019-05-26','1')
 INSERT INTO Urlop(dataod,datado,uzytkownik_id) VALUES('2019-05-27','2019-06-02','2')
@@ -52,6 +52,12 @@ INSERT INTO Urlop(dataod,datado,uzytkownik_id) VALUES('2019-06-10','2019-06-16',
 SELECT * FROM Uzytkownik;
 SELECT * FROM Urlop;
 SELECT * FROM Zespol;
+
+
+ALTER TABLE Uzytkownik ADD adres_email VARCHAR(200) ;
+UPDATE Uzytkownik SET adres_email = 'mail@gmail.com' WHERE ID = 1;
+UPDATE Uzytkownik SET adres_email = 'email@example.com' WHERE ID = 2;
+UPDATE Uzytkownik SET adres_email = 'imejl@int.pl' WHERE ID = 3;
 
 -- Hash MD5 do hasła:password
 --5F4DCC3B5AA765D61D8327DEB882CF99
