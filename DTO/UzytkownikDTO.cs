f43d29c28086d3b7a7a8925a30602c9916c4954f
Paweł <example@mail.com>
2019-05-16 09:58:05 +0200

    Dodanie swagger, podpiecie bazy, podstawowe DTO

---
+++ DTO/UzytkownikDTO.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Freet.DTO
+{
+    public class UzytkownikDTO
+    {
+        public Int64 Id { get; set; }
+        public string Login { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+
+    }
+}
